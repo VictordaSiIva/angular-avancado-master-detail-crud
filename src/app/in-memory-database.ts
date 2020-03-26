134cf06d4bd98c409722e0f69cd6cbf88c7c87f9
Victor da Silva <Victor10019@hotmail.com>
2020-03-26 17:19:33 -0300

    adicionando model de categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,10 +1,12 @@
 import {InMemoryDbService} from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
+
 
 
 // tslint:disable-next-line: class-name
 export class inMemomyDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
         {id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa'},
         {id: 1, name: 'Saude', description: 'Plano de Saude e Remedios'},
         {id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc'},
